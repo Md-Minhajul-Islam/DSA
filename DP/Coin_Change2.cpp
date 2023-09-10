283b13c22b101d4e1bd27d2d0d2be5a60f04117c
Minhaj <minhajulislammuhammad@gmail.com>
2023-09-11 00:50:37 +0600

    dp

---
+++ DP/Coin_Change2.cpp
@@ -0,0 +1,25 @@
+// Q:
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3+5;
+const int INF = 1e9;
+int coin[N], dp[N][N];
+int minCoin(int n, int s)
+{
+    if(s == 0) return 0;
+    if(n < 0) return INF;
+    if (dp[n][s] != -1) return dp[n][s];
+    if(s >= coin[n])
+        return dp[n][s] = min(1+minCoin(n, s-coin[n]), minCoin(n-1, s));
+    else return dp[n][s] = minCoin(n-1, s);
+}
+int main()
+{
+    memset(dp, -1, sizeof(dp));
+    int n, total;
+    cin >> n >> total;
+    for(int i = 0; i < n; i++) cin >> coin[i];
+    cout << minCoin(n-1, total) << "\n"; // min num of coin to make total
+    return 0;
+}
+// unbounded knapsack + subset sum
